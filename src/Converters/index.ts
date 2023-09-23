5a79521b73ff59407771eb2e62a6b26ee6a70cbe
Kamioda <aimegu2014@gmail.com>
2023-09-23 16:18:21 +0900

    １モジュールに変換コードを集約

---
+++ src/Converters/index.ts
@@ -0,0 +1,15 @@
+import { closeCause } from "./closeCause";
+import { correct } from "./correct";
+import { hide } from "./hide";
+import { kind } from "./kind";
+import { latest } from "./latest";
+import { process as converProcess } from "./process";
+
+export const convert = {
+    close_cause: closeCause,
+    correct: correct,
+    hide: hide,
+    kind: kind,
+    latest: latest,
+    process: converProcess
+};
