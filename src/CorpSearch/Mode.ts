46dd7461b805aadbce031ceac6c3aac79362206e
Kamioda <aimegu2014@gmail.com>
2023-09-24 16:52:06 +0900

    値に説明を追加

--- src/CorpSearch/Mode.ts
+++ src/CorpSearch/Mode.ts
@@ -1,4 +1,10 @@
 export enum CorpSearchMode {
+    /**
+     * 前方一致検索
+     */
     Match_Front = 1,
+    /**
+     * 部分一致検索
+     */
     Match_Part = 2,
 }
--- src/CorpSearch/Type.ts
+++ src/CorpSearch/Type.ts
@@ -1,6 +1,18 @@
 export enum CorpSearchType {
+    /**
+     * 国の機関
+     */
     StateAgency = '01',
+    /**
+     * 地方公共団体
+     */
     LocalGovernment = '02',
+    /**
+     * 国内設立法人
+     */
     RegisteredCorpEstablishedInJP = '03',
+    /**
+     * 外国会社等・その他
+     */
     Others = '04',
 }
